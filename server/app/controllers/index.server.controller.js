ff98e19eddc7559bbd6fcf9ea9084ba97a5d7cc5
Shyam <shyampanchal95@gmail.com>
2019-04-18 23:01:43 -0400

    removes server

--- server/app/controllers/index.server.controller.js
+++
@@ -1,4 +0,0 @@
-// Create a new 'render' controller method
-exports.render = function (req, res) {
-    res.json({ message: 'controller working' });
-};
--- server/config/env/production.js
+++
@@ -1,6 +0,0 @@
-// Set the 'development' environment configuration object
-//    db: 'mongodb://localhost:27017/comp308project',
-module.exports = {
-    db: 'mongodb+srv://panchall:password@123@cluster0-9oddw.gcp.mongodb.net/comp308project?retryWrites=true',
-    sessionSecret: 'productionSessionSecret'
-};
